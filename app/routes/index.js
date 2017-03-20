cc0b02486cf0567242c6e7243a0a3184513e8b6f
Cassie Musolf and Jiwon Kang <cassie@>
2017-03-20 10:37:39 -0700

    finish writing the save method.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-  return this.store.findAll('rental');
-},
+    return this.store.findAll('rental');
+  },
 
   actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
-      rental.destroyRental();
+      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
